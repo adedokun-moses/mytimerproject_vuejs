f012cfe90b2321e430cb7860693447c4434c2df8
adedokun moses <adedokunmoses.1450@gmail.com>
2022-06-28 05:33:53 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Moses Reaction Timer</h1>
+  <button class="btn" @click="start"  :disabled="isPlaying" >Play</button>
+
+  <FirstBlock v-if="isPlaying" :delay="delay" @end="endGame" />
+<!--   <p v-if="showResults">reactionTime {{score}} ms </p> -->
+   <Results v-if="showResults" :score ="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import FirstBlock from './components/block_mode.vue'
+   import Results  from './components/result_block.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+      name: 'App',
+      components: { FirstBlock, Results },
+
+      data(){
+        return{
+          isPlaying: false,
+          delay: null,
+          score: null, 
+          showResults: false
+        }
+      },
+
+      methods:{
+        start(){
+            this.delay = 2000 + Math.random() * 5000
+            this.isPlaying = true 
+            this.showResults = false
+           // console.log(this.delay)
+        },
+
+        endGame(reactionTime){
+          this.score = reactionTime
+          this.isPlaying = false
+          this.showResults = true
+        }
+      }
   }
-}
 </script>
 
 <style>
@@ -20,7 +47,17 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
-</style>
+
+.btn{
+  padding: 20px;
+  /* background: green; */
+  color: red;
+  border-radius: 10px;
+  outline: none;
+  border: none;
+  width: 40%;
+}
+</style> 
